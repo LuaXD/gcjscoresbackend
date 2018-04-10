4ede5dc0e7b22944bed2b68552f2351ec68503e5
Lua <xdaddisxd@gmail.com>
2018-04-10 01:04:56 -0400

    change base 64 parser for native js

--- qualification.js
+++ qualification.js
@@ -1,30 +1,30 @@
 const https = require('https');
-const base64 = require('base-64');
 const fs = require('fs');
-const USERS_IN_ROUND = 24000;
+const USERS_IN_ROUND = 24500;
 var end = 0;
-
+var global = this;
 (function getQualificationScores() {
   var scores = [];
   var min_rank = 1;
   var num_consecutive_users = 100
-  while(min_rank < USERS_IN_ROUND) {
-    min_rank += 100;
+  for (let min_rank = 1; min_rank < USERS_IN_ROUND; min_rank += 100) {
     setTimeout(() => {
+      console.log(min_rank);
       var payload = { 
         "min_rank": min_rank,
         "num_consecutive_users": num_consecutive_users 
       }
-      var encodedData = base64.encode(JSON.stringify(payload));
+      var encodedData = Buffer.from(JSON.stringify(payload)).toString('base64');
       const url = 'https://codejam.googleapis.com/scoreboard/00000000000000cb/poll?p=' + encodedData;
       https.get(url, res => {
-        var str = '';
         if (res.statusCode == 200) {
+          var str = '';
           res.on('data', function (chunk) {
             str += chunk;
           });
           res.on('end', function () {
-            const data = JSON.parse(base64.decode(str));
+            var b64 = Buffer.from(str, 'base64').toString();
+            const data = JSON.parse(b64);
             data['user_scores'].forEach(score => {
               scores.push({
                 country: score.country,
@@ -34,9 +34,9 @@ var end = 0;
                 task_info: score.task_info
               });
             });
+            
             end++;
             if (end === parseInt(USERS_IN_ROUND / 100)) {
-              console.log('my end ', scores);
               let resultString = 'const qualification = ';
               resultString = resultString + JSON.stringify(scores);
               resultString = resultString + ';';
